70103f34c7be9602916d3218cd4d5cf6563677d1
xarn <arnaud.dagnelies@gmail.com>
2016-07-06 10:19:18 +0000

    ...

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='pysos',
-    version='1.1.1',
+    version='1.2.0',
+    py_modules=['pysos'],
+    scripts=['pysos.py'],
     description='Simple Object Storage - Persistent dicts and lists for python.',
-    packages=["pysos"],
     url='https://github.com/dagnelies/pysos',
     author='Arnaud Dagnelies',
     author_email='arnaud.dagnelies@gmail.com',
