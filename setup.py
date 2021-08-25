67c9317b9992e31be10718f19120e921f98388b6
Arnaud Dagnelies <arnaud.dagnelies@gmail.com>
2021-08-25 12:50:55 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pysos',
-    version='1.2.3',
+    version='1.2.4',
     py_modules=['pysos'],
     scripts=['pysos.py'],
     description='Simple Object Storage - Persistent dicts and lists for python.',
