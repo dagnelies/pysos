f8b2c463610bff27a09aae4b83bacee84197d365
Arnaud Dagnelies <arnaud.dagnelies@gmail.com>
2022-12-10 13:18:21 +0100

    v1.3.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pysos',
-    version='1.2.9',
+    version='1.3.0',
     py_modules=['pysos'],
     scripts=['pysos.py'],
     description='Simple Object Storage - Persistent dicts and lists for python.',
